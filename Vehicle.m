57e4637e74ead2a4e3575af57b56d18f122eee5f
onnot <onnot@Baby>
2023-03-14 17:23:19 +0100

    Rewrote traction_calculation to make things more readable. Added Vehicle class to reduce the amount of parameters passed to functions. Added Torque function

---
+++ Vehicle.m
@@ -0,0 +1,14 @@
+classdef Vehicle
+    properties
+        Mass
+        friction_coef
+        Area_front
+        drag_coef
+        wheel_radius
+        gear_ratio
+        delta
+        efficiency
+    end
+end
+
+% alpha,V_kph,M,g,f_r,rho,A_f,C_d,V_w_ms
